1f88e6453c2338e5a0d404075300fb0c6f5e5a2f
Momo <75737534+MTsocute@users.noreply.github.com>
2023-05-21 02:29:26 +0800

    第一部分
    扩充了一入门没有的知识
    我们细节注意字符串的处理和 sys 实现操作终端交互的功能

---
+++ 第一部分/Chapter6/Project/pw.py
@@ -0,0 +1,32 @@
+#! /opt/homebrew/bin/python3
+import sys
+import pyperclip
+
+PASSWORDS = {
+    'email': 'F7minlBDDuvMJuxESSKHFhTxFtjVB6',
+    'blog': 'VmALvQyKAxiVH5G8v01if1MLZF3sdt',
+    'luggage': '12345'
+}
+
+
+""" 
+    获取外面一个参数，这个参数是账号，然后返回对应的密码到剪贴板 
+    sys.argv 列表中:
+        第一项总是一个字符串，它包含程序的文件名('pw.py')。
+        第二项应该是第一个命令行参数
+"""
+
+if len(sys.argv) < 2:   # 如果我们忘记输入参数[账号]，那就是少输入一个参数
+    print("您缺少了 [账号] 这个参数！")
+    print('使用方法: python3 pw.py [所需的账号] - 就会返回对应的密码到剪贴板')
+    sys.exit()
+
+account = sys.argv[1]   # 显示第二项：命令行参数
+print(f'检测用户输入到：{account}')    # 把我们命令行输入的参数打印出来
+
+
+if account in PASSWORDS.keys():
+    pyperclip.copy(PASSWORDS[account])
+    print(f'{account} 密码已经拷贝到剪贴板当中了!')
+else:
+    print("检无此账号！！！")
